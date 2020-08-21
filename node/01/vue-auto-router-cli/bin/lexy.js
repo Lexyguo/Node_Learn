0592e34518da5f0ea446d7913d4e3c40c1368159
lexy_guo <1241963177@qq.com>
2020-08-21 14:52:16 +0800

    添加强制格式化自动化操作

--- node/01/vue-auto-router-cli/bin/lexy.js
+++ node/01/vue-auto-router-cli/bin/lexy.js
@@ -3,5 +3,6 @@ const program = require('commander');
 program.version(require('../package').version)
 program.command('init <name>').description('init project').action(require('../lib/init'))
 program.command('refresh').description('refresh routers').action(require('../lib/refresh'))
+program.command('serve').description('serve').action(require('../lib/serve'))
 
 program.parse(process.argv)
