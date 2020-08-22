7130294a311d4820e094414dcbf1d83cca344e89
Lexy <1241963177@qq.com>
2020-08-22 10:23:56 +0800

    finish compose task(ex02)

--- node/ex02/index.js
+++ node/ex02/index.js
@@ -3,7 +3,15 @@ module.exports.compose = middlewares => {
         return dispatch(0)
         function dispatch(i) {
             // ##BEGIN## 代码已加密
-
+            let fn = middlewares[i]
+            if (!fn) {
+                return Promise.resolve()
+            }
+            return Promise.resolve(
+                fn(function next() {
+                    return dispatch(i + 1)
+                })
+            )
             // ##END##
         }
     }
