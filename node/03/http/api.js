49e86cf3b40b7abfe3fb4af574c7d956fefa8075
Lexy <1241963177@qq.com>
2020-08-23 16:59:16 +0800

    使用http-proxy-middleware 代理

--- node/03/http/api.js
+++ node/03/http/api.js
@@ -12,20 +12,20 @@ const app = http
             });
         } else if (method == "GET" && url == "/api/users") {
             res.setHeader("Content-Type", "application/json");
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000')
+            // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000')
             // 预检options中和/users接⼝中均需添加
-            res.setHeader('Access-Control-Allow-Credentials', 'true');
+            // res.setHeader('Access-Control-Allow-Credentials', 'true');
             // 设置cookie
             res.setHeader('Set-Cookie', 'cookie1=123')
             res.end(JSON.stringify([{ name: "tom", age: 20 }]));
         } else if (method == "OPTIONS" && url == "/api/users") {
-            res.writeHead(200, {
-                "Access-Control-Allow-Origin": "http://localhost:3000",
-                "Access-Control-Allow-Headers": "X-Token,Content-Type",
-                'Access-Control-Allow-Credentials': 'true',
-                "Access-Control-Allow-Methods": "PUT"
-            });
-            res.end();
+            // res.writeHead(200, {
+            //     "Access-Control-Allow-Origin": "http://localhost:3000",
+            //     "Access-Control-Allow-Headers": "X-Token,Content-Type",
+            //     'Access-Control-Allow-Credentials': 'true',
+            //     "Access-Control-Allow-Methods": "PUT"
+            // });
+            // res.end();
         }
     });
 
--- node/03/http/proxy.js
+++ node/03/http/proxy.js
@@ -1,5 +1,9 @@
 const express = require('express')
+// http-proxy-middleware
+// 0.x.x版本的引用方式 const proxy = require('http-proxy-middleware');
+// 1.0.0版本的引用方式
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const app = express()
 app.use(express.static(__dirname + '/'))
-
+app.use('/api', createProxyMiddleware({ target: 'http://localhost:3000' }))
 module.exports = app
